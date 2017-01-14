ee5fe84b6850ee77bd1191f0bf685a787302dd56
rewanth1997 <rewanth1997@gmail.com>
2017-01-14 22:01:42 +0000

    Sending post requests

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,16 @@ var fbLogin = require('../lib/api/fbAppLogin');
 var test = require('../lib/api/test1');
 
 /* GET home page. */
+/*
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
+});*/
 
 router.get('/ok', function(req, res) {
 	res.send('asjkldf');
 })
 
-
+router.use('/', fbLogin);
 router.use('/facebook', fbLogin);
 router.use('/test', test);
 
