c3376851d0b525398b59e2c16cf27c6c042a00f0
rewanth1997 <rewanth1997@gmail.com>
2017-01-14 16:34:30 +0000

    API for facebook app request and fetching music list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var fbLogin = require('../lib/api/fbAppLogin');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/ok', function(req, res) {
+	res.send('asjkldf');
+})
+
+router.use('/facebook', fbLogin);
 
 module.exports = router;
